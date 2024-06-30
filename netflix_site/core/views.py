0a6f90a84015ca7d90184ed3ac8a4ac68f7bc90f
GorkemKurtkaya <gorkem.kurtkaya@yahoo.com>
2024-06-30 16:28:07 +0300

    sign

--- netflix_site/core/views.py
+++ netflix_site/core/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
 
 # Create your views here.
 
@@ -9,4 +11,28 @@ def login(request):
     return render(request, 'login.html')
 
 def signup(request):
-    return render(request, 'signup.html')
+    if request.method == 'POST':
+        email = request.POST['email']
+        username = request.POST['username']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email already taken...')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username already taken...')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, password=password, email=email)
+                user.save()
+                #Giriş yapma
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+                return redirect('/')
+        else:
+            messages.info(request, 'Password not matching...')
+            return redirect('signup')
+    else:
+        return render(request, 'signup.html')
